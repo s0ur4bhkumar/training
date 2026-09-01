ee43bb9a49a92d457d29f4195da823f46a5679c1
s0ur4bhkumar <soul65917@gmail.com>
2026-09-01 12:58:28 +0530

    re-organize files

---
+++ intern_training/week01/day01/scripts/README.md
@@ -0,0 +1,11 @@
+## Script for archiving the projects and loggin
+
+#### How to use
+- Make the script executable by using the command "chmod +x script.sh"
+- Run the script by using the command "sh script.sh"
+- After running the script, you need to enter the ammount of archives you want to keep
+- The script will only keep the last n (entered by the user) and remove the rest
+
+
+#### Note
+The script follows the specific Paths.The paths can be changed in the scriptitself.
---
+++ intern_training/week01/day01/scripts/script.sh
@@ -0,0 +1,38 @@
+# !/bin/bash
+
+Date=$(date +%F)
+LogFile='./logs/backup.logs'
+dest='./archives'
+backup='./projects'
+
+echo "Enter the number of files to be kept"
+
+read n_files
+
+log_message(){
+
+  echo "$(date '+%Y-%m-%d %H:%M:%S') [$1] $2" >> "$LogFile"
+}
+
+
+for f in "$backup"/*;
+do 
+  name=$(basename $f)
+  tar -czvf $dest/"$name-$Date".tar.gz $f
+  log_message "INFO" "backup created for $name"
+done
+
+total_projects=$(find ""$backup/* -maxdepth 0 -type d | wc -l)
+
+i=0
+
+for f in "$dest"/*;
+do
+  name=$(basename $f)
+  if [ "$i" -lt $((total_projects - n_files)) ]; then
+    echo "removing $f from archives"
+  log_message "INFO" "removinr $name from archives"
+    rm -rf "$f"
+    ((i++))
+  fi
+done
