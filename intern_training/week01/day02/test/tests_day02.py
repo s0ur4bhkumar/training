12bc6fb6f9acfde970921036c87f85b29cb6b25a
s0ur4bhkumar <soul65917@gmail.com>
2026-09-02 11:09:12 +0530

    feat: fix test cases and import error

--- intern_training/week01/day02/test/tests_day02.py
+++ intern_training/week01/day02/test/tests_day02.py
@@ -1,3 +1,11 @@
+"""
+module for test cases
+"""
+
+import sys
+
+sys.path.insert(0, "/home/sourabh/work/training/intern_training/week01/day02/utils")
+
 import unittest
 
 from utils.collection import dedupe, frequencies, groupby
@@ -5,13 +13,17 @@ from utils.text import clean_text, count_chars, tokenize
 
 
 class TestTextUtils(unittest.TestCase):
+    """
+    class to test the module text.py
+    """
+
     def test_clean_text(self):
         self.assertEqual(clean_text("  HELLO  "), "hello")
 
     def test_count_chars(self):
         self.assertEqual(
             count_chars("hello world"),
-            {"h": 1, "e": 1, "l": 3, "o": 2, "w": "1", "r": 1, "d": 1},
+            {"h": 1, "e": 1, "l": 3, "o": 2, "w": 1, "r": 1, "d": 1},
         )
 
     def test_tokenize(self):
@@ -19,11 +31,15 @@ class TestTextUtils(unittest.TestCase):
 
 
 class TestCollections(unittest.TestCase):
+    """
+    class to test the module collection.py
+    """
+
     def test_frequencies(self):
         self.assertEqual(frequencies(["a", "b", "a"]), {"a": 2, "b": 1})
 
     def test_dedupe(self):
-        self.assertEqual(dedupe(["a", "b", "b"]), ["a", "b"])
+        self.assertEqual(dedupe(["a", "b", "b"]).sort(), ["a", "b"].sort())
 
     def test_group_by(self):
         self.assertEqual(
